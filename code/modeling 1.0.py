024d1d94afb5035c195b8f098732956590fcf669
confusedolive <jeronimomarbella@gmail.com>
2021-04-20 05:00:06 +0100

    more visuals

--- code/modeling 1.0.py
+++ code/modeling 1.0.py
@@ -24,23 +24,18 @@ from mlxtend.classifier      import EnsembleVoteClassifier
 from sklearn.ensemble        import AdaBoostClassifier, GradientBoostingClassifier, StackingClassifier
 from sklearn.naive_bayes     import GaussianNB
 from sklearn.neighbors       import KNeighborsClassifier
-from sklearn.model_selection import GridSearchCV
-
+from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold, cross_val_score
+from sklearn.utils           import compute_class_weight
 
 data_path = r'C:\Users\jeron\OneDrive\Desktop\903group\data\framingham.csv'
 
 data = pd.read_csv(data_path)
-print(len(data))
-print(data['TenYearCHD'].value_counts())
 data_heart = data.copy()
 data_heart.dropna(inplace=True)
-print(len(data_heart))
-print(data_heart['TenYearCHD'].value_counts())
-
-data.drop('currentSmoker',axis=1, inplace=True)
+data.drop(columns=['currentSmoker', 'education'],axis=1, inplace=True)
 
 output = 'TenYearCHD'
-features = ['male', 'age', 'education',
+features = ['male', 'age',
         'cigsPerDay', 'BPMeds', 'prevalentStroke',
         'prevalentHyp', 'diabetes', 'totChol', 'sysBP',
         'diaBP', 'BMI', 'heartRate', 'glucose']
@@ -50,13 +45,10 @@ not_standard = ['male', 'BPMeds', 'prevalentStroke',
 need_standard = [x for x in features if x not in not_standard]
 features_to_scale = data_heart[need_standard]
 
-scaler =StandardScaler()
-normalize = MinMaxScaler()
-
-features_scaled = scaler.fit_transform(features_to_scale.values)
+normalize =MinMaxScaler(feature_range=(0,1))
+features_scaled = normalize.fit_transform(features_to_scale.values)
 
 data_heart[need_standard] = features_scaled
-
 X = data_heart[features]
 y = data_heart[output]
 #boruta py recommends using pruned forest with a depth between 3-7
@@ -91,41 +83,40 @@ def plot_conf(model):
 
 ####################################################################################################
 
-over = SMOTE(sampling_strategy=0.2)
-under = RandomUnderSampler(sampling_strategy=0.8)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35, stratify=y)
-X_train, y_train = over.fit_resample(X_train, y_train)
-X_train,y_train = under.fit_resample(X_train, y_train)
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 results_acc = {}
 results_f1  = {}
 ######################################logistic regression###############################################################
 ###paremeter tuning for logistic regression
 params = [
-        {'penalty': ['l1'],'C': np.logspace(-4, 4, 20),
-        'solver': [ 'liblinear','saga'],
+        {'penalty': ['l1'],'C': np.logspace(-4, 4, 50),
+        'solver': [ 'liblinear','saga'],'class_weight':['balanced'],
         'max_iter':[100, 1000, 1500,5000]},
-        {'penalty': ['l2'],'C': np.logspace(-4, 4, 20),
-         'solver': ['newton-cg','sag','lbfgs'],
+        {'penalty': ['l2'],'C': np.logspace(-4, 4, 50),
+         'solver': ['newton-cg','sag','lbfgs'],'class_weight':['balanced'],
          'max_iter':[100, 1000, 1500,5000]},
-        {'penalty': ['elasticnet'],'C': np.logspace(-4, 4, 20),
+        {'penalty': ['elasticnet'],'class_weight':['balanced'],'C': np.logspace(-4, 4, 50),
          'solver': ['saga'], 'max_iter':[100, 1000, 1500,5000],
          'l1_ratio':[0.5]}]
 
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=42)
 log = LogisticRegression()
-grid = GridSearchCV(log, param_grid=params, cv= 10, verbose=2, n_jobs= -1)
-b = grid.fit(X_train, y_train)
-print(b.best_estimator_)
+grid = GridSearchCV(log, params, cv=cv)
+grid.fit(X, y)
+print(grid.best_estimator_)
 
-logmodel = LogisticRegression(C=11.288378916846883, penalty='l1', solver='saga')
-logmodel.fit(X_train, y_train)
+weights = {0:0.5, 1:0.95}
+log = LogisticRegression(C=0.0011, class_weight='balanced', penalty='l2',
+                   solver='lbfgs')
+log.fit(X_train,y_train)
+print(log.predict(X_test))
+print(classification_report(y_train, log.predict(X_train)))
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=42)
+scores = cross_val_score(log, X_test, y_test, scoring='recall', cv=cv, n_jobs=-1)
 
-print(classification_report(y_test, logmodel.predict(X_test)))
-f1scor   = metrics.f1_score(y_test, logmodel.predict(X_test))
-accscore = logmodel.score(X_test, y_test)
-print(f1scor, accscore)
+print(scores.mean())
+
 
 results_acc['logmodel'] = accscore
 results_f1['logmodel']  = f1scor
@@ -157,10 +148,10 @@ results_acc['svc'] = accscorsv
 results_f1['svc']  = f1scorsv
 #########################Boosting#################################
 
-svc1 = SVC(kernel='linear', probability=True, C=100, gamma=1)
-ada = AdaBoostClassifier(n_estimators=5, base_estimator=svc1)
-grad_boost = GradientBoostingClassifier(n_estimators=10)
-xgb = XGBClassifier(max_depth=5, learning_rate=0.001, use_label_encoder=False)
+log = LogisticRegression(solver='lbfgs', class_weight='balanced')
+ada = AdaBoostClassifier(n_estimators=5, base_estimator=log)
+grad_boost = GradientBoostingClassifier(n_estimators=100)
+xgb = XGBClassifier(max_depth=8, learning_rate=0.001, use_label_encoder=False)
 
 ensemble = EnsembleVoteClassifier(clfs = [ada, grad_boost, xgb], voting='hard')
 
